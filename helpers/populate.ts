79757ec97523f1bb17fd2d61fbeae724cb7fd281
Aditya <adityav0705@gmail.com>
2024-03-04 19:40:18 +0530

    updated query

--- helpers/populate.ts
+++ helpers/populate.ts
@@ -5,29 +5,46 @@ if (process.env.NODE_ENV !== 'production') {
 import { db } from '../models';
 import { cleanDb } from '../helpers/testHelpers';
 import fetch from 'node-fetch';
+import { Ship, Mission } from '../common/types/ship';
 
 const populate = async () => {
   await cleanDb();
   console.log('Populating database...');
 
-  const ships = await fetch('https://spacex-production.up.railway.app/api/graphql', {
+  const ships: Ship[] = await fetch('https://spacex-production.up.railway.app/api/graphql', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: '{ ships { id name image class active } }' }),
+    body: JSON.stringify({ query: '{ ships { id name image class active missions { flight name } } }' }),
   })
     .then(res => res.json())
-    .then(data => data.data.ships);
+    .then(data => (data as { data: { ships: Ship[] } }).data.ships);
 
-  await Promise.all(
-    ships.map((ship: any) => {
+  const dbData = await Promise.all(
+    ships.map(ship => {
       return db.Ship.create({
         active: ship.active,
         name: ship.name,
-        class: ship.class,
+        class: String(ship.class),
         image: ship.image,
       });
     }),
   );
+
+  const missions: Promise<Mission>[] = ships.reduce((acc: Promise<Mission>[], ship, ind: number) => {
+    if (ship.missions) {
+      const shipMissions = ship.missions.map(mission => {
+        return db.Mission.create({
+          flight: mission.flight,
+          name: mission.name || 'mission',
+          shipId: dbData[ind].id,
+        });
+      });
+      return acc.concat(shipMissions as Promise<Mission>[]);
+    }
+    return acc;
+  }, []);
+
+  await Promise.all(missions);
 
   await db.sequelize.close();
 };
